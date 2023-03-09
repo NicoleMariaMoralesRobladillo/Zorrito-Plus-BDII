a28fff79d09e7885ff40e011f05b4e4e42e0ba1f
Nicole Morales <moralesrobladillon@gmail.com>
2023-03-09 11:21:18 -0500

    Corrigiendo link de borrar queja

--- src/components/QuejaUsuario.vue
+++ src/components/QuejaUsuario.vue
@@ -12,7 +12,7 @@ export default {
   },
   methods: {
     async eliminarQueja() {
-      let api = "http://localhost:8080/quejas/eliminarBD" + this.$props.id;
+      let api = "http://localhost:8080/quejas/eliminarBD/" + this.$props.id;
       await axios.get(api).then(
         (response) => {
           let verificador = response.data;
